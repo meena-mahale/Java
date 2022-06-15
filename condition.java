e2a6eada38b5fc586ca406bc5aa2cb9f01d4f367
meena-mahale <103952009+meena-mahale@users.noreply.github.com>
2022-06-15 22:54:55 +0530

    if else

---
+++ condition.java
@@ -0,0 +1,17 @@
+public class condition
+{
+	public static void main (String[] args)
+	{
+		int a =10;
+		int b=20;
+		if(a>b)
+		{
+			System.out.println("a greater than b");	
+		}
+		else
+		{
+			System.out.println("a not greater than b");	
+		}
+	}
+	
+}
